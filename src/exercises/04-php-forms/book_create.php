6de2eab1afd209ac651a2e6317c4d8fc3e6d1860
Jessica Murpy Harte <N00254548@student.iadt.ie>
2026-02-09 12:52:01 +0000

    09/02/26

--- src/exercises/04-php-forms/book_create.php
+++ src/exercises/04-php-forms/book_create.php
@@ -105,7 +105,7 @@ $formats = [
         <div class="form-group">
             <label for="author">Author:</label>
             <!-- TODO: Repopulate author field                               -->
-            <input type="text" id="author" name="author" value="">
+            <input type="text" id="author" name="author" value="<?= old ('author'); ?>">
 
             <!-- TODO: Display error message if author validation fails      -->
 
@@ -142,7 +142,7 @@ $formats = [
         <div class="form-group">
             <label for="year">Year:</label>
             <!-- TODO: Repopulate year field                                 -->
-            <input type="text" id="year" name="year" value="">
+            <input type="text" id="year" name="year" value="<?= old ('year'); ?>">
 
             <!-- TODO: Display error message if year validation fails        -->
 
@@ -154,7 +154,7 @@ $formats = [
         <div class="form-group">
             <label for="isbn">ISBN:</label>
             <!-- TODO: Repopulate ISBN field                                 -->
-            <input type="text" id="isbn" name="isbn" value="">
+            <input type="text" id="isbn" name="isbn" value="<?= old ('isbn'); ?>">
 
             <!-- TODO: Display error message if ISBN validation fails        -->
 
@@ -191,7 +191,7 @@ $formats = [
         <div class="form-group">
             <label for="description">Description:</label>
             <!-- TODO: Repopulate description field                          -->
-            <textarea id="description" name="description" rows="5"></textarea>
+            <textarea id="description" name="description" rows="5"><?= old ('description'); ?></textarea>
 
             <!-- TODO: Display error message if description validation fails -->
 
